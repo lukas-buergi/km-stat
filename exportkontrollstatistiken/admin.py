de0a7f0d82511d66b9c7e233cf0d9c7245da744b
Lukas <lubuergi@student.ethz.ch>
2019-03-04 12:25:16 +0100

    admin interface

--- exportkontrollstatistiken/admin.py
+++ exportkontrollstatistiken/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Uebersetzungen, Kontrollregimes, Exportkontrollnummern, Geschaeftstypen, Geschaeftsrichtungen, Laender, QuellenGeschaefte, QuellenProbleme, Geschaefte, ProblemArtenGesetz, ProblemArten, Probleme
+
+admin.site.register(Uebersetzungen)
+admin.site.register(Kontrollregimes)
+admin.site.register(Exportkontrollnummern)
+admin.site.register(Geschaeftstypen)
+admin.site.register(Geschaeftsrichtungen)
+admin.site.register(Laender)
+admin.site.register(QuellenGeschaefte)
+admin.site.register(QuellenProbleme)
+admin.site.register(Geschaefte)
+admin.site.register(ProblemArtenGesetz)
+admin.site.register(ProblemArten)
+admin.site.register(Probleme)
