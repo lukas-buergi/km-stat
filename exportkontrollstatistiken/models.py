9fa0620d0668585c48754e725f6f6cb525001e95
Lukas <lubuergi@student.ethz.ch>
2019-03-29 14:10:29 +0100

    started entering more data, made and applied migrations

--- exportkontrollstatistiken/models.py
+++ exportkontrollstatistiken/models.py
@@ -19,11 +19,32 @@ class Uebersetzungen(models.Model):
 class Bewilligungstypen(models.Model):
 	""" Ob es für das Geschäft oder die Geschäfte eine (oder mehrere) Einzelbewilligungen gebraucht hat, oder nicht. Für Kriegsmaterial braucht es immer eine Einzelbewilligung, sonst gibt es eine Liste von Ländern, für die es keine braucht. Wie heisst die andere Art von Bewilligung? Siehe 514.51 12ff"""
 	name = models.ForeignKey(Uebersetzungen, on_delete=models.PROTECT)
+	
+	def __str__(self):
+		if(self.name != ""):
+			return(str(self.name))
+		else: # Ich glaube nicht dass das eine gute Lösung ist, andererseits sollte der Fall eh nicht eintreten. TODO
+			return("Bewilligungstypen " + self.pk)
+	
+	class Meta:
+		verbose_name = 'Bewilligungstyp'
+		verbose_name_plural = 'Bewilligungstypen'
 
 class Geschaeftsrichtungen(models.Model):
 	""" Ausfuhr, Vermittlung, etc. Sehr theoretisch, habe bisher nur Ausfuhr gesehen. """
 	name = models.ForeignKey(Uebersetzungen, on_delete=models.PROTECT)
 	""" Ausfuhr, Vermittlung, etc. Sehr theoretisch, habe bisher nur Ausfuhr gesehen. """
+	
+	def __str__(self):
+		if(self.name != ""):
+			return(str(self.name))
+		else: # Ich glaube nicht dass das eine gute Lösung ist, andererseits sollte der Fall eh nicht eintreten. TODO
+			return("Geschaeftsrichtungen " + self.pk)
+	
+	class Meta:
+		verbose_name = 'Geschäftsrichtung'
+		verbose_name_plural = 'Geschäftsrichtungen'
+
 
 class Laender(models.Model):
 	""" Liste der Länder, in die exportiert wird. Vielleicht braucht das noch lat. und long., aber vielleicht eher nicht?"""
@@ -34,6 +55,10 @@ class Laender(models.Model):
 
 	def __str__(self):
 		return(self.code)
+	
+	class Meta:
+		verbose_name = 'Land'
+		verbose_name_plural = 'Länder'
 
 class QuellenGeschaefte(models.Model):
 	""" Offizielle Quelle für jedes Geschäft. Eine Quelle ist normalerweise Quelle für viele Geschäfte."""
@@ -52,13 +77,27 @@ class QuellenGeschaefte(models.Model):
 			return(str(self.name))
 		else: # Ich glaube nicht dass das eine gute Lösung ist, andererseits sollte der Fall eh nicht eintreten. TODO
 			return("QuellenGeschaefte " + self.pk)
-		
+	
+	class Meta:
+		verbose_name = 'Quelle für Geschäfte'
+		verbose_name_plural = 'Quellen für Geschäfte'
+
 class QuellenProbleme(models.Model):
 	""" Quellen für jedes Problem, als Link. """
 	link = models.URLField()
 	linkText = models.CharField(max_length=200)
 	sprache = models.CharField(max_length=2)
 	""" Code für die Sprache der Quelle, DE, FR, IT, EN. """
+	
+	def __str__(self):
+		if(self.linkText != ""):
+			return(str(self.linkText))
+		else: # Ich glaube nicht dass das eine gute Lösung ist, andererseits sollte der Fall eh nicht eintreten. TODO
+			return("QuellenProbleme " + self.pk)
+	
+	class Meta:
+		verbose_name = 'Quelle für Probleme'
+		verbose_name_plural = 'Quellen für Probleme'
 
 class GueterArten(models.Model):
 	""" Warum das Gut reguliert wird.
@@ -67,6 +106,16 @@ class GueterArten(models.Model):
 	* Dual use
 	* Sanktionsgüter - Das sollte wohl normalerweise ausgenommen werden von den Totalen, oder?"""
 	name = models.ForeignKey(Uebersetzungen, on_delete=models.PROTECT)
+	
+	def __str__(self):
+		if(self.name != ""):
+			return(str(self.name))
+		else: # Ich glaube nicht dass das eine gute Lösung ist, andererseits sollte der Fall eh nicht eintreten. TODO
+			return("GueterArten " + self.pk)
+	
+	class Meta:
+		verbose_name = 'Güterart'
+		verbose_name_plural = 'Güterarten'
 
 class Kontrollregimes(models.Model):
 	""" Die verschiedenen Kontrollregimes:
@@ -84,7 +133,17 @@ class Kontrollregimes(models.Model):
 	name = models.ForeignKey(Uebersetzungen, on_delete=models.PROTECT)
 	gueterArt = models.ForeignKey(GueterArten, on_delete=models.PROTECT)
 	inkrafttreten = models.DateField()
-	aufgehobenwerden = models.DateField(blank=True)
+	aufgehobenwerden = models.DateField(blank=True, null=True)
+	
+	def __str__(self):
+		if(self.name != ""):
+			return(str(self.name))
+		else: # Ich glaube nicht dass das eine gute Lösung ist, andererseits sollte der Fall eh nicht eintreten. TODO
+			return("Kontrollregimes " + self.pk)
+	
+	class Meta:
+		verbose_name = 'Kontrollregime'
+		verbose_name_plural = 'Kontrollregimes'
 
 class Exportkontrollnummern(models.Model):
 	""" Enthält die Anhänge mit den Beschreibungen der Nummern. Die Beschreibung wird erst bei Gelegenheit eingelesen, aber zumindest die Nummern selbst braucht es bevor ein Geschäft mit dieser Nummer eingetragen wird. """
@@ -94,6 +153,16 @@ class Exportkontrollnummern(models.Model):
 	""" Die Nummer. """
 	beschreibung = models.ForeignKey(Uebersetzungen, on_delete=models.PROTECT, blank=True)
 	""" Die Beschreibung aus der Liste. Ich bin nicht sicher ob die übersetzt werden, falls nicht bleiben die anderen Spalten der fehlenden Sprachen halt leer. Ist zumindest vorerst optional."""
+	
+	def __str__(self):
+		if(self.nummer != ""):
+			return(str(self.nummer))
+		else: # Ich glaube nicht dass das eine gute Lösung ist, andererseits sollte der Fall eh nicht eintreten. TODO
+			return("Exportkontrollnummern " + self.pk)
+	
+	class Meta:
+		verbose_name = 'Exportkontrollnummer'
+		verbose_name_plural = 'Exportkontrollnummern'
 
 class Geschaefte(models.Model):
 	""" Ein Eintrag steht für ein oder mehrere Geschäfte, je nach Datenlage. Die Daten gibt es von
@@ -103,9 +172,7 @@ class Geschaefte(models.Model):
 	nummer = models.PositiveIntegerField(blank=True, null=True)
 	""" Offizielle Geschäftsnummer, wo vorhanden. Bei Statistiken wo diese Nummer nicht vorhanden ist steht ein Eintrag für mehrere Geschäfte """
 	endempfaengerstaat = models.ForeignKey(Laender, on_delete=models.PROTECT)
-	""" Ist bei allen verwendeten Statistiken Endempfängerstaat, nicht Bestimmungsland, oder? - TODO: Genau herausfinden und hier verbessern."""
-	art = models.ForeignKey(GueterArten, on_delete=models.PROTECT)
-	
+	""" Ist bei allen verwendeten Statistiken Endempfängerstaat, nicht Bestimmungsland, oder? - TODO: Genau herausfinden und hier verbessern."""	
 	bewilligungstyp = models.ForeignKey(Bewilligungstypen, on_delete=models.PROTECT)
 	""" Einzelbewilligung oder sonst etwas? """
 	richtung = models.ForeignKey(Geschaeftsrichtungen, on_delete=models.PROTECT)
@@ -120,11 +187,19 @@ class Geschaefte(models.Model):
 	""" Das Ende des Geschäftes. Geschäfte können über mehrere Jahren gehen stand irgendwo auf der Secowebseite (macht ja Sinn). Ich schätze das sieht man bei neueren Statistiken an der Nummer und bei älteren wurde das Geschäft wahrscheinlich nur in einem Jahr einbezogen nehme ich an (keine Ahnung). """
 	quelle = models.ForeignKey(QuellenGeschaefte, on_delete=models.PROTECT)
 	""" Die offizielle Quelle für den Eintrag. Ich glaube es gibt die immer nur auf Deutsch, sonst müsste man das noch anpassen um auch die anderen anzubieten. """
+	
+	class Meta:
+		verbose_name = 'Geschäft'
+		verbose_name_plural = 'Geschäfte'
 
 class ProblemArtenGesetz(models.Model):
 	""" Liste der Gesetzesabschnitte, die Gründe enthalten, warum Exporte verboten werden. """
 	gesetz = models.CharField(max_length=30)
 	""" Gesetz in Kurzschreibweise wie "514.511 5 IId". Dann kann man automatisch eine sprachabhängige längere Variante generieren. """
+	
+	class Meta:
+		verbose_name = 'Gesetz das Problemart stützt'
+		verbose_name_plural = 'Gesetze die Problemarten stützten'
 
 class ProblemArten(models.Model):
 	""" Kategorisierung der Probleme unter anderem nach Erwähnung in KMV. Einträge sollten mindestens sein:
@@ -147,6 +222,10 @@ class ProblemArten(models.Model):
 	""" Ob das Problem im Gesetz als Ausschlusskriterium bezeichnet wird. """
 	faktor = models.FloatField()
 	""" Ein Faktor um Geschäfte grob nach Schlimme zu sortieren. Die Sortierung wird nicht öffentlich so bezeichnet oder als Sortierreihenfolge angeboten, sondern nur dann verwendet, wenn sonst noch keine Reihenfolge ausgewählt wurde. Dafür wird der Umfang des Geschäfts mit dem Faktor multipliziert. Zwischen 1 und 2 inklusiv. Falls ausschlusskriterium wahr ist, gibt es einen zusätzlichen Faktor 2. """
+	
+	class Meta:
+		verbose_name = 'Problemart'
+		verbose_name_plural = 'Problemarten'
 
 class Probleme(models.Model):
 	""" Jeder Eintrag enthält einen konkreten Grund, warum man in ein bestimmtes Land in einem Zeitraum nicht hätte exportieren sollten. Wird in einem Diagramm den tatsächlichen Exporten gegenüber gestellt. """
@@ -165,3 +244,7 @@ class Probleme(models.Model):
 	""" Zusammenfassung in wenigen Sätzen. """
 	quellen = models.ManyToManyField(QuellenProbleme)
 	""" Quellen, zum Beispiel Zeitungsberichte. Vorzugsweise mehrere für jede Sprache. """
+	
+	class Meta:
+		verbose_name = 'Problem'
+		verbose_name_plural = 'Probleme'
