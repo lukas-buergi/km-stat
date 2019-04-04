1e54c3118311bf79769b13d3cd78c373f4c38654
Lukas <lubuergi@student.ethz.ch>
2019-04-04 18:26:13 +0200

    aufgeräumt

--- exportkontrollstatistiken/urls.py
+++ exportkontrollstatistiken/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('api/g/<granularity>/<countries>/<types>/<int:year1>/<int:year2>/<sortby>/<int:perpage>/<int:pageNumber>', views.gapi, name='gapi'),
+    path('api/g/<granularity>/<countries>/<types>/<int:year1>/<int:year2>/<sortBy>/<int:perPage>/<int:pageNumber>', views.gapi, name='gapi'),
     path('table', views.table, name='table'),
     path('worldmap', views.worldmap, name='worldmap'),
     path('', views.index, name='index'),
--- exportkontrollstatistiken/views.py
+++ exportkontrollstatistiken/views.py
@@ -10,9 +10,7 @@ import datetime
 
 # TODO: Where does this belong?
 class apiParam():
-    """Parst die Parameter und stellt sie praktischer zur Verfügung. TODO
-        * Für die Typen eine Liste der Uebersetzungen-Objekte, die Namen von angefragten Typen sind.
-        * Für die Länder eine Liste von Länder-Objekten, deren Geschäfte angefragt wurden"""
+    """Parst die Parameter und stellt sie in praktischeren Formen zur Verfügung bzw. stoppt wenn sie falsch sind."""
     
     # granularity
     granularities = {
@@ -44,14 +42,17 @@ class apiParam():
     }
     sortBy=None
     
-    def __init__(self, granularity, countries, types, year1, year2, sortby, perpage, pageNumber):
+    def __init__(self, granularity, countries, types, year1, year2, sortBy, perPage, pageNumber):
+        self.perPage=perPage
+        self.pageNumber=pageNumber
+        
         if(granularity not in self.granularities):
             raise(ValueError)
         self.granularity=self.granularities[granularity]
         
-        if(sortby not in self.sortByChoices):
+        if(sortBy not in self.sortByChoices):
             raise(ValueError)
-        self.sortBy=self.sortByChoices[sortby]
+        self.sortBy=self.sortByChoices[sortBy]
 
         if(self.granularity=="summed" and self.sortBy not in ["-umfang", "umfang"]):
             raise(ValueError)
@@ -98,17 +99,101 @@ class apiParam():
         if(self.granularity=="summedPerYear" and not self.countriesSingle and self.sortBy in ["-beginn", "beginn"]):
             raise(NotImplementedError)
 
+def individual(p, queryset, writer):
+    queryset = queryset.order_by(p.sortBy)
+    queryset = queryset[p.perPage*(p.pageNumber-1):p.perPage*p.pageNumber]
+    
+    titleRow=["Datum", "Art", "EKN", "Umfang"]
+    if(not p.countriesSingle):
+        titleRow = ["Ländercode", "Land"] + titleRow
+    writer.writerow(titleRow)
+    
+    for g in queryset:
+        row=[g.beginn, g.exportkontrollnummer.kontrollregime.gueterArt.name.de, g.exportkontrollnummer, g.umfang]
+        if(not p.countriesSingle):
+            row = [g.endempfaengerstaat.code, g.endempfaengerstaat.name.de] + row
+        writer.writerow(row)
+
+def summedPerYear(p, queryset, writer):
+    """Return transactions summed per year. Algorithm:
+        * sort by country, year
+        * sum up consecutive entries until country or year changes
+        * sort according to p.sortBy, but only certain combinations are valid:
+            * date asc+des, for a single country (that's the original order after the summing algorithm)
+            * value"""
     
 
-def gapi(request, granularity, countries, types, year1, year2, sortby, perpage, pageNumber):
-    """Die view, die die Geschäftsdaten an das Frontend liefert. Das Format ist csv, weil das deutlich datensparsamer ist als der eingebaute JSON serializer (Faktor >2, soweit ich das gesehen habe).
+    if(p.sortBy in ["beginn","-beginn"]):
+        firstSort=p.sortBy
+    else:
+         # in that case it can be either because later it's sorted again anyway
+        firstSort="beginn"
+        
+    queryset = queryset.order_by("endempfaengerstaat", firstSort)
+
+    for g in queryset:
+        print(g)
+
+    sums=dict()
+    curCountry = None
+    curYear = None
+    for g in queryset:
+        if(g.endempfaengerstaat!=curCountry or g.beginn.year!=curYear):
+            curCountry = g.endempfaengerstaat
+            curYear = g.beginn.year
+            curSum = g.umfang
+            sums[str(curCountry) + str(curYear)] = [curSum, curYear, curCountry]
+        else:
+            sums[str(curCountry) + str(curYear)][0] += g.umfang
+
+    reverse=None
+    if(p.sortBy == "-umfang"):
+        reverse=True
+    elif(p.sortBy == "umfang"):
+        reverse=False
+    
+    if(reverse==None):
+        order=sums
+    else:
+        order=sorted(sums, key=lambda key : sums.get(key)[0], reverse=reverse)
+    
+    for country in order:
+        writer.writerow([sums[country][2].code, sums[country][2].name.de, sums[country][1], sums[country][0]])
+
+def summed(p, queryset, writer):
+    """Sum transactions per country. Algorithm:
+        * sort by country
+        * sum up consecutive entries until the country changes
+        * sort by value"""
+    
+    queryset = queryset.order_by("endempfaengerstaat")
+    sums=dict()
+    curCountry = None
+    for g in queryset:
+        if(g.endempfaengerstaat!=curCountry):
+            curCountry=g.endempfaengerstaat
+            curSum=g.umfang
+            sums[curCountry] = curSum
+        else:
+            sums[curCountry]+=g.umfang
+
+    if(p.sortBy=="-umfang"):
+        reverse=True
+    else:
+        reverse=False
+    
+    for country in sorted(sums, key=sums.get, reverse=reverse):
+        writer.writerow([country.code, country.name.de, sums[country]])
+
+def gapi(request, granularity, countries, types, year1, year2, sortBy, perPage, pageNumber):
+    """Die view, die die Geschäftsdaten an das Frontend liefert.
     Die Parameter erklären sich zusammen mit der apiParam Klasse."""
 
     # input validation and preparation
     try:
-        p=apiParam(granularity, countries, types, year1, year2, sortby, perpage, pageNumber)
+        p=apiParam(granularity, countries, types, year1, year2, sortBy, perPage, pageNumber)
     except ValueError:
-        raise
+        raise # TODO: This line is not for production.
         return(HttpResponse("Invalid parameter."))
     
     # prepare queryset as far as possible
@@ -124,90 +209,11 @@ def gapi(request, granularity, countries, types, year1, year2, sortby, perpage, 
 
     # write response
     if(p.granularity=="summed"):
-        # sort by country
-        # sum up consecutive entries
-        # until the country changes
-        # sort by value TODO: Complain if other sort requested
-        queryset = queryset.order_by("endempfaengerstaat")
-        sums=dict()
-        curCountry = None
-        for g in queryset:
-            if(g.endempfaengerstaat!=curCountry):
-                curCountry=g.endempfaengerstaat
-                curSum=g.umfang
-                sums[curCountry] = curSum
-            else:
-                sums[curCountry]+=g.umfang
-
-        if(p.sortBy=="-umfang"):
-            reverse=True
-        else:
-            reverse=False
-        
-        for country in sorted(sums, key=sums.get, reverse=reverse):
-            writer.writerow([country.code, country.name.de, sums[country]])
-                
-        
+        summed(p, queryset, writer)
     elif(p.granularity=="summedPerYear"):
-        # sort by country, year
-        # sum up consecutive entries
-        # until country or year changes
-        # sort according to p.sortBy, but only certain combinations are valid:
-        # * date asc+des, for a single country (that's the original order after the summing algorithm)
-        # * value, descending (sort like for p.granularity=="summed")
-
-        if(p.sortBy in ["beginn","-beginn"]):
-            firstSort=p.sortBy
-        else:
-             # in that case it can be either because later it's sorted again anyway
-            firstSort="beginn"
-            
-        queryset = queryset.order_by("endempfaengerstaat", firstSort)
-
-        for g in queryset:
-            print(g)
-
-        sums=dict()
-        curCountry = None
-        curYear = None
-        for g in queryset:
-            if(g.endempfaengerstaat!=curCountry or g.beginn.year!=curYear):
-                curCountry = g.endempfaengerstaat
-                curYear = g.beginn.year
-                curSum = g.umfang
-                sums[str(curCountry) + str(curYear)] = [curSum, curYear, curCountry]
-            else:
-                sums[str(curCountry) + str(curYear)][0] += g.umfang
-
-        reverse=None
-        if(p.sortBy == "-umfang"):
-            reverse=True
-        elif(p.sortBy == "umfang"):
-            reverse=False
-        
-        if(reverse==None):
-            order=sums
-        else:
-            order=sorted(sums, key=lambda key : sums.get(key)[0], reverse=reverse) # does this work? TODO
-        
-        for country in order:
-            writer.writerow([sums[country][2].code, sums[country][2].name.de, sums[country][1], sums[country][0]])
-
-        
+        summedPerYear(p, queryset, writer)
     elif(p.granularity=="individual"):
-        queryset = queryset.order_by(p.sortBy)
-        queryset = queryset[perpage*(pageNumber-1):perpage*pageNumber]
-        
-        titleRow=["Datum", "Art", "EKN", "Umfang"]
-        if(not p.countriesSingle):
-            titleRow = ["Ländercode", "Land"] + titleRow
-        writer.writerow(titleRow)
-        
-        for g in queryset:
-            row=[g.beginn, g.exportkontrollnummer.kontrollregime.gueterArt.name.de, g.exportkontrollnummer, g.umfang]
-            if(not p.countriesSingle):
-                row = [g.endempfaengerstaat.code, g.endempfaengerstaat.name.de] + row
-            writer.writerow(row)
+        individual(p, queryset, writer)
 
     return(response)
 
