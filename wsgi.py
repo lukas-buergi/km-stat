2f1b82e93dc7f9474adc79a1ed0604630bfa4fb7
Lukas <lubuergi@student.ethz.ch>
2019-06-03 13:36:47 +0200

    another try at deploying to this hosting provider

--- wsgi.py
+++ wsgi.py
@@ -1,1 +1,20 @@
-kriegsmaterialch/wsgi.py
+"""
+WSGI config for kriegsmaterialch project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
+"""
+
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__),
+                                                'kriegsmaterialch')))
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kriegsmaterialch.settings')
+
+application = get_wsgi_application()
